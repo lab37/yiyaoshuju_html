bf9e02fb8621d1992320ee3bba62a4d8f743d1a7
lab37 <379718363@qq.com>
2018-09-30 13:49:18 +0800

    V3.0.0

--- main.go
+++ main.go
@@ -24,7 +24,8 @@ func chaxun(w http.ResponseWriter, r *http.Request) {
   biaoge := keys["neirong"][0]
   yaopinname := strings.TrimFunc(keys["yaopinname"][0],unicode.IsSpace)
   
-  if biaoge == "zhongbiao" { 
+  switch biaobe {
+  case "zhongbiao": 
 		shengchanqiye := strings.TrimFunc(keys["shengchanqiye"][0],unicode.IsSpace)
 		type Rst struct {
 		  Tym string
@@ -65,7 +66,7 @@ func chaxun(w http.ResponseWriter, r *http.Request) {
 			r, _ := json.Marshal(rsts)
 			fmt.Fprintf(w, string(r))
 	    }  
-    } else if biaoge == "yibao" { 
+    case "yibao": 
 		type Rst struct {
 		  Fldm string
 		  Fl1 string
@@ -98,7 +99,7 @@ func chaxun(w http.ResponseWriter, r *http.Request) {
 			r, _ := json.Marshal(rsts)
 			fmt.Fprintf(w, string(r))
 	    }  
-    } else if biaoge == "jcbc" { 
+    case "jcbc":
 		shengchanqiye := strings.TrimFunc(keys["shengchanqiye"][0],unicode.IsSpace)
 		type Rst struct {
 		  Tym string
@@ -137,24 +138,25 @@ func chaxun(w http.ResponseWriter, r *http.Request) {
 			r, _ := json.Marshal(rsts)
 			fmt.Fprintf(w, string(r))
 	    }  
-    } else if biaoge == "rlzy" {
-	    shengchanqiye := strings.TrimFunc(keys["shengchanqiye"][0],unicode.IsSpace)
-		dishi := strings.TrimFunc(keys["dishi"][0],unicode.IsSpace)
+    case "factoryh":
+	    factory := strings.TrimFunc(keys["factory"][0],unicode.IsSpace)
+		city := strings.TrimFunc(keys["city"][0],unicode.IsSpace)
+		county := strings.TrimFunc(keys["county"][0],unicode.IsSpace)
+		variety := strings.TrimFunc(keys["variety"][0],unicode.IsSpace)
+		hospital := strings.TrimFunc(keys["hospital"][0],unicode.IsSpace)
 		type Rst struct {
-		  Tym string
-		  Jx string
-		  Gg string
-		  Scqy string
-		  Ds string
-		  Yy string
-		  Fzr string
-		  Lxfs string
-		  Rylx string
-		  Bz string
+		  Factory string
+		  City string
+		  County string
+		  Hospital string
+		  Variety string
+		  Name string
+		  Tel string
+		  Remark string
 		}
 		
-		if yaopinname=="" && shengchanqiye=="" && dishi=="" {
-		    rst := []Rst {{"你未输入任何有效的字符串", " ", " ", " ", " ", " ", " ", " ", " ", " "}}
+		if yaopinname=="" && factory=="" && city=="" && county=="" & hospital =="" {
+		    rst := []Rst {{"你未输入任何有效的字符串", " ", " ", " ", " ", " ", " ", " "}}
 		    r, _ := json.Marshal(rst)
 		    fmt.Fprintf(w, string(r))
 		} else {
@@ -162,31 +164,149 @@ func chaxun(w http.ResponseWriter, r *http.Request) {
 			db, err := sql.Open("sqlite3", "./db/yiyaoshuju.db")
 			defer db.Close()
 			checkErr(err)
-			rows, err := db.Query("SELECT tym, jx, gg, scqy, ds, yy, fzr, lxfs, ryls, bz FROM renliziyuan WHERE tym LIKE ? AND scqy LIKE ? And ds LIKE ?", 
-								  "%" + yaopinname + "%", "%" + shengchanqiye + "%", "%" + dishi + "%")
+			rows, err := db.Query("SELECT factory, city, county, hospital, variety, name, tel, remark FROM factoryh WHERE variety LIKE ? AND factory LIKE ? And city LIKE ? And county LIKE ? And hospital LIKE ?", 
+								  "%" + yaopinname + "%", "%" + factory + "%", "%" + city + "%","%" + county + "%","%" + hospital + "%")
 			checkErr(err)
 			defer rows.Close()
 			for rows.Next() {
 			  rst := Rst{}
-			  err = rows.Scan(&rst.Tym, &rst.Jx, &rst.Gg, &rst.Scqy, &rst.Ds, &rst.Yy, &rst.Fzr, &rst.Lxfs, &rst.Rylx, &rst.Bz)
+			  err = rows.Scan(&rst.Factory, &rst.City, &rst.County, &rst.Hospital, &rst.Variety, &rst.Name, &rst.Tel, &rst.Remark)
 			  checkErr(err)
 			  rsts = append(rsts, rst)
 			}
 			r, _ := json.Marshal(rsts)
 			fmt.Fprintf(w, string(r))
-	    }  
+	    }
+		
+	case "terminalh":
+	    factory := strings.TrimFunc(keys["factory"][0],unicode.IsSpace)
+		city := strings.TrimFunc(keys["city"][0],unicode.IsSpace)
+		county := strings.TrimFunc(keys["county"][0],unicode.IsSpace)
+		variety := strings.TrimFunc(keys["variety"][0],unicode.IsSpace)
+		hospital := strings.TrimFunc(keys["hospital"][0],unicode.IsSpace)
+		type Rst struct {
+		  Name string
+		  Tel string
+		  City string
+		  County string
+		  Hospital string
+		  Variety string
+		  Factory string
+		  Remark string
+		}
+		
+		if yaopinname=="" && factory=="" && city=="" && county=="" & hospital =="" {
+		    rst := []Rst {{"你未输入任何有效的字符串", " ", " ", " ", " ", " ", " ", " "}}
+		    r, _ := json.Marshal(rst)
+		    fmt.Fprintf(w, string(r))
+		} else {
+			rsts := []Rst{}
+			db, err := sql.Open("sqlite3", "./db/yiyaoshuju.db")
+			defer db.Close()
+			checkErr(err)
+			rows, err := db.Query("SELECT name, tel, city, county, hospital, variety, factory, remark FROM terminalh WHERE variety LIKE ? AND factory LIKE ? And city LIKE ? And county LIKE ? And hospital LIKE ?", 
+								  "%" + yaopinname + "%", "%" + factory + "%", "%" + city + "%","%" + county + "%","%" + hospital + "%")
+			checkErr(err)
+			defer rows.Close()
+			for rows.Next() {
+			  rst := Rst{}
+			  err = rows.Scan(&rst.Name, &rst.Tel, &rst.City, &rst.County, &rst.Hospital, &rst.Variety, &rst.Factory, &rst.Remark)
+			  checkErr(err)
+			  rsts = append(rsts, rst)
+			}
+			r, _ := json.Marshal(rsts)
+			fmt.Fprintf(w, string(r))
+	    }
+	case "channelh":
+	    channel := strings.TrimFunc(keys["channel"][0],unicode.IsSpace)
+		city := strings.TrimFunc(keys["city"][0],unicode.IsSpace)
+		county := strings.TrimFunc(keys["county"][0],unicode.IsSpace)
+		type Rst struct {
+		  Channel string
+		  Type string
+		  Region string
+		  Hospital string
+		  Department string
+		  Post string
+		  Name string
+		  Tel string
+		  Remark string
+		}
+		
+		if channel=="" && city=="" && county=="" {
+		    rst := []Rst {{"你未输入任何有效的字符串", " ", " ", " ", " ", " ", " ", " ", " "}}
+		    r, _ := json.Marshal(rst)
+		    fmt.Fprintf(w, string(r))
+		} else {
+			rsts := []Rst{}
+			db, err := sql.Open("sqlite3", "./db/yiyaoshuju.db")
+			defer db.Close()
+			checkErr(err)
+			rows, err := db.Query("SELECT channel, type, region, hospital, department, post, name, tel, remark FROM channelh WHERE channel LIKE ? AND city LIKE ? And county LIKE ?", 
+								  "%" + channel + "%", "%" + channel + "%", "%" + city + "%","%" + county + "%")
+			checkErr(err)
+			defer rows.Close()
+			for rows.Next() {
+			  rst := Rst{}
+			  err = rows.Scan(&rst.Channel, &rst.Type, &rst.Region, &rst.Hospital, &rst.Department, &rst.Post, &rst.Name, &rst.Tel, &rst.Remark)
+			  checkErr(err)
+			  rsts = append(rsts, rst)
+			}
+			r, _ := json.Marshal(rsts)
+			fmt.Fprintf(w, string(r))
+	    }
+	case "terminals":
+	    hospital := strings.TrimFunc(keys["hospital"][0],unicode.IsSpace)
+		city := strings.TrimFunc(keys["city"][0],unicode.IsSpace)
+		county := strings.TrimFunc(keys["county"][0],unicode.IsSpace)
+		type Rst struct {
+		  Hospital string
+		  Type string
+		  Legalman string
+		  Class string
+		  Economy string
+		  City string
+		  County string
+		  Address string
+		  Works string
+		  Beds string
+		  Remark string
+		}
+		
+		if hospital=="" && city=="" && county=="" {
+		    rst := []Rst {{"你未输入任何有效的字符串", " ", " ", " ", " ", " ", " ", " ", " ", , " ", , " "}}
+		    r, _ := json.Marshal(rst)
+		    fmt.Fprintf(w, string(r))
+		} else {
+			rsts := []Rst{}
+			db, err := sql.Open("sqlite3", "./db/yiyaoshuju.db")
+			defer db.Close()
+			checkErr(err)
+			rows, err := db.Query("SELECT hospital, type, legalman, class, economy, city, county, address,  works, beds, remark FROM terminals WHERE hospital LIKE ? AND city LIKE ? And county LIKE ?", 
+								  "%" + hospital + "%", "%" + city + "%","%" + county + "%")
+			checkErr(err)
+			defer rows.Close()
+			for rows.Next() {
+			  rst := Rst{}
+			  err = rows.Scan(&rst.Hospital, &rst.Type, &rst.Legalman, &rst.Class, &rst.Economy, &rst.City, &rst.County, &rst.Address, &rst.Works, &rst.Beds, &rst.Remark)
+			  checkErr(err)
+			  rsts = append(rsts, rst)
+			}
+			r, _ := json.Marshal(rsts)
+			fmt.Fprintf(w, string(r))
+	    }
 	    
-    } else {
+    default:
 	    type Rst struct {
 		  Tym string
 		}
 	    rst := Rst{"你未输入任何有效的字符串"}
 		r, _ := json.Marshal(rst)
 		fmt.Fprintf(w, string(r))
-	}
 	
+	
+  }
 }
-
 func main() {
   server := http.Server{
 	  Addr: "127.0.0.1:8090",
